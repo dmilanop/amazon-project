d708a1496252439fdb3446546f1eb4e528417d2c
dmilanop <dmilanop28@gmail.com>
2022-08-11 19:16:23 -0400

    Cree la vista Home.jsx, elimine App.js y App.css, por ultimo se modifico el layout

--- src/layout.js
+++ src/layout.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import App from "./views/App.js"
+import Home from "./views/Home.jsx";
 import injectContext from "./store/appContext.js";
 
 const Layout = () => {
@@ -10,7 +10,7 @@ const Layout = () => {
         <div>
             <BrowserRouter basename= {basename}>
                 <Routes>
-                    <Route path="/" element={<App/>} />
+                    <Route path="/" element={<Home/>} />
                     <Route element={<h1> Not found! </h1>} />
                 </Routes>
             </BrowserRouter>

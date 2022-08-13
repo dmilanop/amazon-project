996904a00f7270949df110885f69936840e94ef3
dmilanop <dmilanop28@gmail.com>
2022-08-13 00:27:44 -0400

    Codigo sin error, se modifico el layout, agregamos la carpeta components y img, creamos en styles Views.css y en views creamos Home2.jsx como prueba de vistas

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Link } from "react-router-dom"
+
+const Navbar = () => {
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <Link to="/" className="navbar-brand" href="#"><h5>Navbar</h5></Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <Link to="/views2" className="nav-link active" aria-current="page" href="#"><h6>Home</h6></Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/views2" className="nav-link" href="#"><h6>Link</h6></Link>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <h6 className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Dropdown
+                                </h6>
+                                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <li><Link to="/views2" className="dropdown-item" href="#"><h6>Action</h6></Link></li>
+                                    <li><Link to="/views2" className="dropdown-item" href="#"><h6>Another action</h6></Link></li>
+                                    <li><hr className="dropdown-divider"/></li>
+                                    <li><Link to="/views2" className="dropdown-item" href="#"><h6>Something else here</h6></Link></li>
+                                </ul>
+                            </li>
+                            <li className="nav-item">
+                                <h6 className="nav-link disabled">Disabled</h6>
+                            </li>
+                        </ul>
+                        <form className="d-flex">
+                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                                <button className="btn btn-outline-success" type="submit">Search</button>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar;
--- src/layout.js
+++ src/layout.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./views/Home.jsx";
+import Navbar from "./components/Navbar.jsx";
 import injectContext from "./store/appContext.js";
+import Home2 from "./views/Home2.jsx";
 
 const Layout = () => {
     const basename = process.env.BASENAME || ""
@@ -9,8 +11,10 @@ const Layout = () => {
     return(
         <div>
             <BrowserRouter basename= {basename}>
+                <Navbar/>
                 <Routes>
                     <Route path="/" element={<Home/>} />
+                    <Route path="/views2" element={<Home2/>} />
                     <Route element={<h1> Not found! </h1>} />
                 </Routes>
             </BrowserRouter>

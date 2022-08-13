79c3d3dad09cd9997d4faf73afd9be13a760efac
dmilanop <dmilanop28@gmail.com>
2022-08-13 02:17:45 -0400

    el navbar esta escuchando un evento y se modifico el diseño del mismo :)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,19 @@
-import React from "react";
-import { Link } from "react-router-dom"
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
+
+    const[disabled, setDisabled] = useState(false)
+
     return (
         <>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <nav className="navbar navbar-light bg-light">
                 <div className="container-fluid">
-                    <Link to="/" className="navbar-brand" href="#"><h5>Navbar</h5></Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
+                    <Link to="/" className="navbar-brand" href="#"><h5>Amazon Project</h5></Link>
+                    <button className="navbar-toggler" disabled={disabled} onClick={() => setDisabled(true)} type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <FontAwesomeIcon icon={faBars} />
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
@@ -33,10 +38,6 @@ const Navbar = () => {
                                 <h6 className="nav-link disabled">Disabled</h6>
                             </li>
                         </ul>
-                        <form className="d-flex">
-                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                                <button className="btn btn-outline-success" type="submit">Search</button>
-                        </form>
                     </div>
                 </div>
             </nav>

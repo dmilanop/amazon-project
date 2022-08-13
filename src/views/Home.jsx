8e8cd63b6f9dacc1b4557f0f1bc1cbc78afc80c6
dmilanop <dmilanop28@gmail.com>
2022-08-13 01:21:39 -0400

    modificado el background de la vista home

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const Home = () => {
     return(
-        <h1>Hello World!</h1>
+        <main>
+            <h1>Hello World!</h1>
+        </main>
+        
     )
 }
 

3a19e5e7b2beed159f9597d287e38aa208816822
dmilanop <dmilanop28@gmail.com>
2022-08-17 19:48:08 -0400

    modificacion del navbar, la vista home2 y estilos

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,10 +11,7 @@ const Navbar = () => {
                     <Link to={"/"} className="navbar-brand">
                         <p className="name-project">Amazon Project </p>
                     </Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                    <div className="collapse navbar-collapse">
                         <ul className="navbar-nav navbar-brand">
                             <li className="nav-item">
                                 <Link to={"/views2"} className="nav-link active" aria-current="page"><p className="p-navbar">Home</p></Link>
--- src/views/Home2.jsx
+++ src/views/Home2.jsx
@@ -6,13 +6,21 @@ const Home2 = () => {
     return (
         <>
             <div className="container">
-                <div className="div-1 card text-white bg-dark mb-3">
-                    <img src={food} className="card-img-top" alt="..."/>
+                <div className="row-card">
+                    <div className="card">
+                        <img src={food} className="card-img-top" alt="..."/>
                         <div className="card-body">
                             <h5 className="card-title">Card title</h5>
-                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         </div>
+                    </div>
+                    <div className="card">
+                        <img src={food} className="card-img-top" alt="..."/>
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>

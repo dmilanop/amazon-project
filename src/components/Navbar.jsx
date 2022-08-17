a3528cb9e92ec7e28916e52d22e841a1405afbd2
dmilanop <dmilanop28@gmail.com>
2022-08-17 18:44:22 -0400

    fuimos agregando mas elementos al navbar, unos estilos a los componentes y a las vista

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,11 +6,30 @@ const Navbar = () => {
 
     return (
         <>
-            <nav className="navbar navbar-light">
+            <nav className="navbar navbar-expand-lg navbar-light">
                 <div className="container-fluid">
-                    <Link to={"/"} className="navbar-brand" href="#">
+                    <Link to={"/"} className="navbar-brand">
                         <p className="name-project">Amazon Project </p>
                     </Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul className="navbar-nav navbar-brand">
+                            <li className="nav-item">
+                                <Link to={"/views2"} className="nav-link active" aria-current="page"><p className="p-navbar">Home</p></Link>
+                            </li>
+                            <li className="nav-item">
+                                <p className="nav-link p-navbar">Features</p>
+                            </li>
+                            <li className="nav-item">
+                                <p className="nav-link p-navbar">Pricing</p>
+                            </li>
+                            <li className="nav-item"> 
+                                <p className="nav-link p-navbar">About us</p>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </nav>
         </>
--- src/views/Home2.jsx
+++ src/views/Home2.jsx
@@ -5,13 +5,15 @@ import food from "../img/food.png"
 const Home2 = () => {
     return (
         <>
-            <div className="div-1 card mb-3">
-                <img src={food} className="card-img-top" alt="..."/>
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-                    </div>
+            <div className="container">
+                <div className="div-1 card text-white bg-dark mb-3">
+                    <img src={food} className="card-img-top" alt="..."/>
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                        </div>
+                </div>
             </div>
         </>
     )

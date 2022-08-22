9ced7c5781f965a7fdcb1c99dc0199466bde56d7
dmilanop <dmilanop28@gmail.com>
2022-08-22 12:12:29 -0400

    agregando estilos al navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,22 +7,22 @@ const Navbar = () => {
 
     return (
         <>
-            <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+            <nav className="navbar navbar-expand-md navbar-dark">
                 <div className="container-fluid">
-                    <Link to={"/"} className="navbar-brand"><FaAmazon/> <span className="text-warning">Amazon Project</span></Link>
+                    <Link to={"/"} className="navbar-brand"><FaAmazon className="nav-logo"/> <span className="nav-title">Amazon Project</span></Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menu" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="menu">
                         <ul className="navbar-nav mx-auto">
                             <li className="nav-item">
-                                <Link to={"/views2"} className="nav-link active">Products</Link>
+                                <Link to={"/views2"} className="nav-link"><span className="link-decoration">Products</span></Link>
                             </li>
                             <li className="nav-item">
-                                <Link to={"/views2"} className="nav-link">Review</Link>
+                                <Link to={"/views2"} className="nav-link"><span className="link-decoration">Review</span></Link>
                             </li>
                             <li className="nav-item">
-                                <Link to={"/views2"} className="nav-link">About us</Link>
+                                <Link to={"/views2"} className="nav-link"><span className="link-decoration">About us</span></Link>
                             </li>
                             <li className="nav-item dropdown">
                                 <Link 
@@ -30,7 +30,7 @@ const Navbar = () => {
                                 className="nav-link dropdown-toggle" 
                                 id="navbarDropdown" 
                                 role="button"
-                                 data-bs-toggle="dropdown">Contact</Link>
+                                 data-bs-toggle="dropdown"><span className="link-decoration">Contact</span></Link>
                                 <ul className="dropdown-menu bg-secondary">
                                     <li>
                                         <a href="https://getbootstrap.com/docs/5.1/components/navbar/" className="dropdown-item" target="_blank" rel="noreferrer noopener">Email</a>
@@ -47,13 +47,13 @@ const Navbar = () => {
                         <hr className="text-white-50"/>
                         <ul className="navbar-nav flex-row flex-wrap text-light">
                             <li className="nav-item col-6 col-md-auto p-3">
-                                <FaGithub /><small className="d-md-none ms-2">Github</small>
+                                <FaGithub className="nav-logo"/><small className="d-md-none ms-2">Github</small>
                             </li>
                             <li className="nav-item col-6 col-md-auto p-3">
-                                <FaInstagram /><small className="d-md-none ms-2">Instagram</small>
+                                <FaInstagram className="nav-logo"/><small className="d-md-none ms-2">Instagram</small>
                             </li>
                             <li className="nav-item col-6 col-md-auto p-3">
-                                <FaLinkedinIn /><small className="d-md-none ms-2">LinkedIn</small>
+                                <FaLinkedinIn className="nav-logo"/><small className="d-md-none ms-2">LinkedIn</small>
                             </li>
                         </ul>
                     </div>
